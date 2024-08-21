1c7d44b2ff7a88cae7cab838c7e2daf659f4838e
Mehdi Assam <mehdi-assam@hotmail.com>
2024-08-21 19:03:06 +0200

    ajout paypal

--- src/Controller/PaypalController.php
+++ src/Controller/PaypalController.php
@@ -7,9 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-class PayPalController extends AbstractController
+class PaypalController extends AbstractController
 {
     public function __construct(private PayPalService $payPalService)
     {
