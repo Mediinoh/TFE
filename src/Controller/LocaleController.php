1b59c6a6dda5823216533d6695aa1badd4056a0a
Mehdi Assam <mehdi-assam@hotmail.com>
2024-10-23 13:08:38 +0200

    ajout commentaires dans le code

--- src/Controller/LocaleController.php
+++ src/Controller/LocaleController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LocaleController extends AbstractController
 {
 
     #[Route('/changeLocale/{locale}', name: 'changeLocale', methods: ['GET'])]
-    public function changeLocale(Request $request, string $locale)
+    public function changeLocale(SessionInterface $session, Request $request, string $locale)
     {
+        // Démarrer manuellement la session (si besoin)
+        if (!$session->isStarted()) {
+            $session->start();
+        }
+
         // Stocke la langue dans la session
         $request->getSession()->set('_locale', $locale);
 
